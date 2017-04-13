3d030a3bdcc55b7db51bca2f08b128e77ca02baa
chenqunming <826237660@qq.com>
2017-04-13 09:49:36 +0800

    android6.0权限使用

--- app/src/main/java/com/cqm/permission/MainActivity.java
+++ app/src/main/java/com/cqm/permission/MainActivity.java
@@ -1,16 +1,8 @@
 package com.cqm.permission;
 
 import android.Manifest;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -18,12 +10,14 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    final static int PERMISSIONS_REQUEST = 111;
+
+    protected PermissionHelper mHelper = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mHelper = new PermissionHelper(this);
 
         Button contacts = (Button) findViewById(R.id.contacts);
         Button msm = (Button) findViewById(R.id.sms);
@@ -31,119 +25,166 @@ public class MainActivity extends AppCompatActivity {
         Button phone = (Button) findViewById(R.id.phone);
         Button storage = (Button) findViewById(R.id.storage);
 
+        mHelper.setOnApplyPermissionListener(new PermissionHelper.OnPermissionListener() {
+            @Override
+            public void onAfterApplyAllPermission(PermissionHelper.PermissionModel model, boolean allow) {
+                if (allow) {
+                    Toast.makeText(MainActivity.this, model.name + "权限已获取！", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, model.name + "权限已被拒绝！", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
         contacts.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                checkPermission(Manifest.permission.READ_CONTACTS);
+                PermissionHelper.PermissionModel model = new PermissionHelper.PermissionModel();
+                model.pemission = Manifest.permission.READ_CONTACTS;
+                model.name = "联系人";
+                mHelper.checkPermission(model);
             }
         });
 
         msm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                checkPermission(Manifest.permission.SEND_SMS);
+                PermissionHelper.PermissionModel model = new PermissionHelper.PermissionModel();
+                model.pemission = Manifest.permission.SEND_SMS;
+                model.name = "短信";
+                mHelper.checkPermission(model);
             }
         });
 
         camera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                checkPermission(Manifest.permission.CAMERA);
+                PermissionHelper.PermissionModel model = new PermissionHelper.PermissionModel();
+                model.pemission = Manifest.permission.CAMERA;
+                model.name = "相机";
+                mHelper.checkPermission(model);
             }
         });
 
         phone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                checkPermission(Manifest.permission.CALL_PHONE);
+                PermissionHelper.PermissionModel model = new PermissionHelper.PermissionModel();
+                model.pemission = Manifest.permission.CALL_PHONE;
+                model.name = "电话";
+                mHelper.checkPermission(model);
             }
         });
 
         storage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                PermissionHelper.PermissionModel model = new PermissionHelper.PermissionModel();
+                model.pemission = Manifest.permission.CALL_PHONE;
+                model.name = "存儲";
+                mHelper.checkPermission(model, true);
             }
         });
 
 
     }
 
-    private void checkPermission(String permission) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this,
-                    permission);
-            //判断是否有权限
-            if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
-
-//                if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
-//                        permission)) {
-//                    //前一次一倍拒绝，再次调起向用户额外解释权限的情况
-//                    Toast.makeText(this,"shouldShowRequestPermissionRationale",Toast.LENGTH_LONG).show();
-//                } else {
-                    Toast.makeText(this,"没有权限，请求权限",Toast.LENGTH_LONG).show();
-                    ActivityCompat.requestPermissions(MainActivity.this,
-                            new String[]{permission},
-                            PERMISSIONS_REQUEST);
-//                }
-            }else {
-                Toast.makeText(this,"已有权限",Toast.LENGTH_LONG).show();
-            }
-        }else{
-            Toast.makeText(this,"不是6.0系统",Toast.LENGTH_LONG).show();
-        }
-    }
+//    private void checkPermission(String permission) {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this,
+//                    permission);
+//            //判断是否有权限
+//            if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+//
+////                if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+////                        permission)) {
+////                    //前一次一倍拒绝，再次调起向用户额外解释权限的情况
+////                    Toast.makeText(this,"shouldShowRequestPermissionRationale",Toast.LENGTH_LONG).show();
+////                } else {
+//                    Toast.makeText(this,"没有"+permission+"权限，请求权限",Toast.LENGTH_LONG).show();
+//                    ActivityCompat.requestPermissions(MainActivity.this,
+//                            new String[]{permission},
+//                            PERMISSIONS_REQUEST);
+////                }
+//            }else {
+//                Toast.makeText(this,permission+" 已有权限",Toast.LENGTH_LONG).show();
+//            }
+//        }else{
+//            Toast.makeText(this,"不是6.0系统",Toast.LENGTH_LONG).show();
+//        }
+//    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        mHelper.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        switch (requestCode) {
+//            case PERMISSIONS_REQUEST: {
+//                if (grantResults.length > 0
+//                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//
+//                    Toast.makeText(this,"允许"+permissions.toString()+"权限",Toast.LENGTH_LONG).show();
+//
+//                } else {
+//                    showAppSettingsDialog(permissions.toString());
+//                }
+//                return;
+//            }
+//        }
+    }
 
-                    Toast.makeText(this,"允许权限",Toast.LENGTH_LONG).show();
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mHelper.onActivityResult(requestCode, resultCode, data);
+//        switch (requestCode) {
+//            case 0:
+//                int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this,
+//                        Manifest.permission.CAMERA);
+//                //判断是否有权限
+//                if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+//                    Toast.makeText(this,"允许"+Manifest.permission.CAMERA+"权限",Toast.LENGTH_LONG).show();
+//
+//                } else {
+//                    finish();
+//                }
+//                break;
+//        }
 
-                } else {
-                    showAppSettingsDialog();
-                }
-                return;
-            }
-        }
     }
 
     /**
      * 显示提示信息
      *
      */
-    private void showAppSettingsDialog() {
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("权限提示");
-        builder.setMessage("当前应用缺少必要权限。请点击\"设置\"-\"权限\"-打开所需权限。");
-
-        // 拒绝, 退出应用
-        builder.setNegativeButton("取消",
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                });
-
-        builder.setPositiveButton("设置",
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                        startAppSettings();
-                    }
-                });
-
-        builder.setCancelable(false);
-
-        builder.show();
-    }
+//    private void showAppSettingsDialog(String permissions) {
+//
+//        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//        builder.setTitle("权限提示");
+//        builder.setMessage("当前应用缺少"+permissions+"权限。请点击\"设置\"-\"权限\"-打开所需权限。");
+//
+//        // 拒绝, 退出应用
+//        builder.setNegativeButton("取消",
+//                new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        dialog.dismiss();
+//                    }
+//                });
+//
+//        builder.setPositiveButton("设置",
+//                new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        dialog.dismiss();
+//                        startAppSettings();
+//                    }
+//                });
+//
+//        builder.setCancelable(false);
+//
+//        builder.show();
+//    }
 
     /**
      *  启动应用的设置
@@ -151,10 +192,26 @@ public class MainActivity extends AppCompatActivity {
      * @since 2.5.0
      *
      */
-    private void startAppSettings() {
-        Intent intent = new Intent(
-                Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-        intent.setData(Uri.parse("package:" + getPackageName()));
-        startActivity(intent);
-    }
+//    private void startAppSettings() {
+////        Intent intent = new Intent(
+////                Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+////        intent.setData(Uri.parse("package:" + getPackageName()));
+////        startActivity(intent);
+//
+//        try {
+//            Intent intent =
+//                    new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:" + getPackageName()));
+//            intent.addCategory(Intent.CATEGORY_DEFAULT);
+//
+//            // Android L 之后Activity的启动模式发生了一些变化
+//            // 如果用了下面的 Intent.FLAG_ACTIVITY_NEW_TASK ，并且是 startActivityForResult
+//            // 那么会在打开新的activity的时候就会立即回调 onActivityResult
+//            // intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//            startActivityForResult(intent, 0);
+//        } catch (Throwable e) {
+//            Toast.makeText(this,e.toString(),Toast.LENGTH_LONG).show();
+//        }
+//    }
+
+
 }
---
+++ app/src/main/java/com/cqm/permission/PermissionHelper.java
@@ -0,0 +1,247 @@
+package com.cqm.permission;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by chenqunming on 2017/4/12.
+ */
+
+public class PermissionHelper {
+
+    /**
+     * 权限申请事件监听
+     */
+    public interface OnPermissionListener {
+
+        /**
+         * 申请所有权限之后的逻辑
+         */
+        void onAfterApplyAllPermission(PermissionModel model, boolean allow);
+    }
+
+    public static class PermissionModel {
+        public String pemission;//申请的权限
+        public String explain;//权限说明
+        public String name;//权限名称
+    }
+
+    private final static String TAG = "PermissionHelper";
+
+    public final static int PERMISSIONS_REQUEST = 98;//权限返回码
+    public final static int SETTING_RESULT_CODE = 99;//应用设置返回码
+
+    private Activity mActivity;//上下文
+
+    private boolean mNeedClose = false;//是否需要关闭当前Activity
+
+    private PermissionModel mPmodel;
+    private OnPermissionListener mOnPermissionListener;//权限申请事件监听
+
+
+    public PermissionHelper(Activity activity) {
+        mActivity = activity;
+    }
+
+    public void setOnApplyPermissionListener(OnPermissionListener onPermissionListener) {
+        mOnPermissionListener = onPermissionListener;
+    }
+
+    /**
+     * 请求权限
+     *
+     * @param model
+     */
+    public void checkPermission(PermissionModel model) {
+        checkPermission(model, false);
+    }
+
+    /**
+     * 请求权限
+     *
+     * @param model
+     * @param needclose
+     */
+    public void checkPermission(PermissionModel model, boolean needclose) {
+        mPmodel = model;
+        mNeedClose = needclose;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            int permissionCheck = ContextCompat.checkSelfPermission(mActivity,
+                    mPmodel.pemission);
+            //判断是否有权限
+            if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+
+//                if (ActivityCompat.shouldShowRequestPermissionRationale(mActivity,
+//                        mPermission)) {
+                //前一次已被拒绝，再次调起向用户额外解释权限的情况
+//                showExplainDialog();
+//                    Toast.makeText(mActivity,"shouldShowRequestPermissionRationale",Toast.LENGTH_LONG).show();
+//                } else {
+                Log.v(TAG, "没有" + mPmodel.name + "权限，请求权限");
+                ActivityCompat.requestPermissions(mActivity,
+                        new String[]{mPmodel.pemission},
+                        PERMISSIONS_REQUEST);
+//                }
+            } else {
+                if (mOnPermissionListener != null) {
+                    mOnPermissionListener.onAfterApplyAllPermission(mPmodel, true);
+                }
+                Log.v(TAG, mPmodel.name + " 已有权限");
+            }
+        } else {
+            Log.v(TAG, "不是6.0系统");
+        }
+    }
+
+
+    /**
+     * 申请权限返回
+     *
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (mOnPermissionListener != null) {
+                        mOnPermissionListener.onAfterApplyAllPermission(mPmodel, true);
+                    }
+                    Log.v(TAG, "允许" + mPmodel.name + "权限");
+
+                } else {
+                    showAppSettingsDialog();
+                }
+                return;
+            }
+        }
+    }
+
+
+    /**
+     * 显示权限说明信息
+     */
+    private void showExplainDialog() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(mActivity);
+        builder.setTitle("权限提示");
+        builder.setMessage(mPmodel.explain);
+        builder.setPositiveButton("确定",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        checkPermission(mPmodel, mNeedClose);
+                    }
+                });
+        builder.setCancelable(false);
+        builder.show();
+    }
+
+    /**
+     * 显示权限提示信息
+     */
+    private void showAppSettingsDialog() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(mActivity);
+        builder.setTitle("权限提示");
+        builder.setMessage("当前应用缺少" + mPmodel.name + "权限。请点击\"设置\"-\"权限\"-打开所需权限。");
+        // 拒绝, 退出应用
+        String cancelStr = "取消";
+        if (mNeedClose) {
+            cancelStr = "退出";
+        }
+        builder.setNegativeButton(cancelStr,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        if (mOnPermissionListener != null) {
+                            mOnPermissionListener.onAfterApplyAllPermission(mPmodel, false);
+                        }
+                        if (mNeedClose) {
+                            mActivity.finish();
+                        }
+                    }
+                });
+
+        builder.setPositiveButton("设置",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        startAppSettings();
+                    }
+                });
+
+        builder.setCancelable(false);
+
+        builder.show();
+    }
+
+    /**
+     * 启动应用的设置
+     */
+    private void startAppSettings() {
+        try {
+            Intent intent =
+                    new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:" + mActivity.getPackageName()));
+            intent.addCategory(Intent.CATEGORY_DEFAULT);
+
+            // Android L 之后Activity的启动模式发生了一些变化
+            // 如果用了下面的 Intent.FLAG_ACTIVITY_NEW_TASK ，并且是 startActivityForResult
+            // 那么会在打开新的activity的时候就会立即回调 onActivityResult
+            // intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            mActivity.startActivityForResult(intent, SETTING_RESULT_CODE);
+        } catch (Throwable e) {
+            Toast.makeText(mActivity, e.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    /**
+     * 应用权限设置返回
+     *
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case SETTING_RESULT_CODE:
+                int permissionCheck = ContextCompat.checkSelfPermission(mActivity,
+                        mPmodel.pemission);
+                //判断是否有权限
+                if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+                    if (mOnPermissionListener != null) {
+                        mOnPermissionListener.onAfterApplyAllPermission(mPmodel, false);
+                    }
+                    if (mNeedClose) {
+                        mActivity.finish();
+                    }
+
+                } else {
+                    if (mOnPermissionListener != null) {
+                        mOnPermissionListener.onAfterApplyAllPermission(mPmodel, true);
+                    }
+                    Log.v(TAG, "允许" + mPmodel.name + "权限");
+                }
+                break;
+        }
+
+    }
+
+
+}
